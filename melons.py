30251b2c45647d425a8802b1c987be8bd50655f4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-07 21:04:14 -0800

    inital commit

---
+++ melons.py
@@ -0,0 +1,109 @@
+"""Classes for melon orders."""
+class AbstractMelonOrder():
+    """A abstract base class that other Melon Orders inherit from."""
+    def __init__(self, species, qty, order_type, tax):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 5
+
+        if self.order_type == "christmas":
+            base_price = base_price * 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic", tax=0.08)
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international melon order."""
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty, "international", tax=0.17)
+        self.country_code = country_code
+
+    def get_total(self):
+        """Overridden get_total method including international flat fee"""
+        return super().get_total() + 3
+
+    def get_country_code(self):
+        """Return the country code."""
+        return self.country_code
+
+
+
+
+
+# class DomesticMelonOrder():
+#     """A melon order within the USA."""
+
+#     def __init__(self, species, qty):
+#         """Initialize melon order attributes."""
+
+#         self.species = species
+#         self.qty = qty
+#         self.shipped = False
+#         self.order_type = "domestic"
+#         self.tax = 0.08
+
+#     def get_total(self):
+#         """Calculate price, including tax."""
+
+#         base_price = 5
+#         total = (1 + self.tax) * self.qty * base_price
+
+#         return total
+
+#     def mark_shipped(self):
+#         """Record the fact than an order has been shipped."""
+
+#         self.shipped = True
+
+
+# class InternationalMelonOrder():
+#     """An international (non-US) melon order."""
+
+#     def __init__(self, species, qty, country_code):
+#         """Initialize melon order attributes."""
+
+#         self.species = species
+#         self.qty = qty
+#         self.country_code = country_code
+#         self.shipped = False
+#         self.order_type = "international"
+#         self.tax = 0.17
+
+#     def get_total(self):
+#         """Calculate price, including tax."""
+
+#         base_price = 5
+#         total = (1 + self.tax) * self.qty * base_price
+
+#         return total
+
+#     def mark_shipped(self):
+#         """Record the fact than an order has been shipped."""
+
+#         self.shipped = True
+
+#     def get_country_code(self):
+#         """Return the country code."""
+
+#         return self.country_code
